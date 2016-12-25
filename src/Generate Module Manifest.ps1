82f9304186ba5af58a093f2b733e21b82944edae
Hilton Giesenow <HG1@za.group.com>
2016-12-25 14:38:41 +0200

    Fixing module manifest in scc

--- src/Generate Module Manifest.ps1
+++ src/Generate Module Manifest.ps1
@@ -1,4 +1,5 @@
-﻿Remove-Item -Path "C:\Development\GitHub\PoShMon\PoShMon\src\0.1.0\Modules\PoShMon.psd1"
+﻿$manifestPath = "C:\Dev\GitHub\PoShMon\PoShMon\src\0.1.0\PoShMon.psd1"
+Remove-Item -Path $manifestPath -ErrorAction SilentlyContinue
 
 $NestedModules = @(
     'PoShMon.Shared\SharedMonitoringModule.psm1'
@@ -7,9 +8,9 @@ $NestedModules = @(
     'PoShMon.Web\WebMonitoringModule.psm1'
 )
 
-New-ModuleManifest -Path "C:\Development\GitHub\PoShMon\PoShMon\src\0.1.0\Modules\PoShMon.psd1" -Guid '6e6cb274-1bed-4540-b288-95bc638bf679' -Author "Hilton Giesenow" -CompanyName "Experts Inside" -FunctionsToExport "*" -Copyright "2016 Hilton Giesenow, All Rights Reserved" -ProjectUri "https://github.com/HiltonGiesenow/PoShMon" -Description "A PowerShell-based server and farm monitoring solution" -ModuleVersion "0.1.0" -NestedModules $NestedModules
+New-ModuleManifest -Path $manifestPath -Guid '6e6cb274-1bed-4540-b288-95bc638bf679' -Author "Hilton Giesenow" -CompanyName "Experts Inside" -FunctionsToExport "'Invoke-OSMonitoring','Invoke-SPMonitoring'" -Copyright "2016 Hilton Giesenow, All Rights Reserved" -ProjectUri "https://github.com/HiltonGiesenow/PoShMon" -Description "A PowerShell-based server and farm monitoring solution" -ModuleVersion "0.1.0"
 
-$t = Test-ModuleManifest -Path "C:\Development\GitHub\PoShMon\PoShMon\src\0.1.0\Modules\PoShMon.psd1"
+$t = Test-ModuleManifest -Path $manifestPath
 
 $t
 
