8140f150358a07f73e728b05a71344b782c632d5
Hilton Giesenow <hilton@giesenow.com>
2016-12-30 17:14:00 +0200

    Up'ing version number

--- src/0.4.0/Tests/Unit/PoShMon.Notifications.Email/Get-EmailOutput.Tests.ps1
+++
@@ -1,5 +0,0 @@
-$rootPath = Join-Path (Split-Path -Parent $MyInvocation.MyCommand.Path) -ChildPath ('..\..\') -Resolve
-Import-Module (Join-Path $rootPath -ChildPath "PoShMon.psd1") -Verbose
-$sutFileName = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests", "")
-$sutFilePath = Join-Path $rootPath -ChildPath "Functions\PoShMon.Notifications.Email\$sutFileName" 
-. $sutFilePath
