b208688aa34625f3d7efe87fdcaf850e6f4bfc90
Hilton Giesenow <hilton@giesenow.com>
2017-01-24 21:11:56 +0200

    updating samples

--- src/Samples/SPMonitoring_Critical.ps1
+++ src/Samples/SPMonitoring_Critical.ps1
@@ -1,6 +1,6 @@
 ﻿Import-Module "C:\Dev\GitHub\PoShMon\src\0.4.0\PoShMon.psd1" -Verbose -Force #This is only necessary if you haven't installed the module into your Modules folder, e.g. via PowerShellGallery / Install-Module
 
-#Alternatively, use
+#Alternatively, use the lines below
 #If (!(Get-module PoShMon))
 #    { Import-Module PoShMon }
 
