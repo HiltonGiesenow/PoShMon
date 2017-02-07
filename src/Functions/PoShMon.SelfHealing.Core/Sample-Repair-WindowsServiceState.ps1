61c938eba738cf4e4ee52f1b3d55b038b2416de2
Hilton Giesenow <HG1@za.group.com>
2017-02-07 13:43:43 +0200

    Adding sample Resolver

---
+++ src/Functions/PoShMon.SelfHealing.Core/Sample-Repair-WindowsServiceState.ps1
@@ -0,0 +1,38 @@
+﻿<#
+
+#>
+Function Repair-WindowsServiceState
+{
+    [CmdletBinding()]
+    Param(
+        [hashtable]$PoShMonConfiguration,
+        [object[]]$PoShMonOutputValues
+    )
+
+    $stoppedServices = $PoShMonOutputValues | Where { $_.SectionHeader -EQ "Windows Service State" -and $_.NoIssuesFound -eq $false }
+ 
+    $repairOutput = @()
+
+    foreach ($groupOutputValue in $stoppedServices.OutputValues)
+    {
+        $serverName = $groupOutputValue.GroupName
+        $services = $groupOutputValue.GroupOutputValues | Where { $_.Highlight.Count -gt 0 }
+
+        $serviceNames = @()
+        foreach ($service in $services)
+            { $serviceNames += $service.Name }
+
+        Invoke-Command -ComputerName $serverName -ScriptBlock {
+            param($serviceNames)
+            $serviceNames | Start-Service
+            $serviceNames | Set-Service -StartupType Automatic #Presumably if it's meant to be running, it should be set to auto start...
+        } -ArgumentList $serviceNames
+ 
+        $repairOutput += @{
+                 "SectionHeader" = "Windows Service State on $serverName"
+                "RepairResult" = "The following sevices were re-started: $serviceNames"
+            }
+    }
+
+    return $repairOutput
+}
