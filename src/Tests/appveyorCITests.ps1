f66502dc9e27951282cca71bcc973cc57ed6fd02
Hilton Giesenow <hilton@giesenow.com>
2017-02-22 20:52:38 +0200

    new appveyor tests file

---
+++ src/Tests/appveyorCITests.ps1
@@ -0,0 +1,57 @@
+$ProjectRoot = $ENV:APPVEYOR_BUILD_FOLDER
+Set-Location $ProjectRoot
+
+#$path = (Split-Path -Parent $MyInvocation.MyCommand.Path)
+
+Import-Module Pester
+
+Invoke-Pester -Path "$ProjectRoot\Tests\Unit" -OutputFormat NUnitXml -OutputFile "$ProjectRoot\RawUnitTestResults.xml" -PassThru | `
+            Export-Clixml -Path "$ProjectRoot\PesterUnitTestResults.xml"
+
+Invoke-Pester -Path "$ProjectRoot\Tests\Integration" -OutputFormat NUnitXml -OutputFile "$ProjectRoot\RawIntegrationTestResults.xml" -PassThru | `
+            Export-Clixml -Path "$ProjectRoot\PesterIntegrationTestResults.xml"
+
+#Show status...
+$AllFiles = Get-ChildItem -Path $ProjectRoot\*Results.xml | Select -ExpandProperty FullName
+"`n`tSTATUS: Finalizing results`n"
+"COLLATING FILES:`n$($AllFiles | Out-String)"
+
+#Upload results for test page
+Get-ChildItem -Path "$ProjectRoot\Raw*TestResults.xml" | Foreach-Object {
+    $Address = "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)"
+    $Source = $_.FullName
+
+    "UPLOADING FILES: $Address $Source"
+
+    (New-Object 'System.Net.WebClient').UploadFile( $Address, $Source )
+}
+
+#What failed?
+$Results = @( Get-ChildItem -Path "$ProjectRoot\Pester*TestResults.xml" | Import-Clixml )
+            
+$FailedCount = $Results |
+    Select -ExpandProperty FailedCount |
+    Measure-Object -Sum |
+    Select -ExpandProperty Sum
+    
+if ($FailedCount -gt 0) {
+
+    $FailedItems = $Results |
+        Select -ExpandProperty TestResult |
+        Where {$_.Passed -notlike $True}
+
+    "FAILED TESTS SUMMARY:`n"
+    $FailedItems | ForEach-Object {
+        $Test = $_
+        [pscustomobject]@{
+            Describe = $Test.Describe
+            Context = $Test.Context
+            Name = "It $($Test.Name)"
+            Result = $Test.Result
+        }
+    } |
+        Sort Describe, Context, Name, Result |
+        Format-List
+
+    throw "$FailedCount tests failed."
+}
