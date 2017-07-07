8e0a78ca1208ef5dcac58aa38f96c3a2ede769e5
Hilton Giesenow <HG1@za.group.com>
2017-07-07 12:51:58 +0200

    updating output for OVF

--- src/Functions/PoShMon.Notifications.OperationValidationFramework/Invoke-OperationVerificationFrameworkScan.ps1
+++ src/Functions/PoShMon.Notifications.OperationValidationFramework/Invoke-OperationVerificationFrameworkScan.ps1
@@ -11,32 +11,54 @@ Function Invoke-OperationVerificationFrameworkScan
     foreach ($outputSection in $TestOutputValues)
     {
     
-        $global:issueFound = $false
-
         Describe $outputSection.SectionHeader {
             It "Should not have any Exceptions" {
                 $outputSection.ContainsKey("Exception") | Should Be $false
             }
 
-            foreach ($outputValue in $outputSection.OutputValues)
-            {
-                #$cleanObject = $outputValue.psobject.Copy()
-                #$cleanObject.psobject.Properties.Remove("Highlight")
-                #$cleanJsonValue = ConvertTo-Json($cleanObject)
-                $actualJsonValue = ConvertTo-Json($outputValue)
-                
-                foreach ($headerKey in $outputSection.OutputHeaders.Keys)
+
+            $isAnItemError = $false
+
+            # Some errors occur -within- the item, e.g. harddrive space below a minimum threshold.
+            # In other tests, the actual presence of values in the output is the error itself, e.g. if items are found in an error log.
+            # The code below checks for that
+            if ($outputSection.NoIssuesFound -eq $false) {
+                foreach ($outputValue in $outputSection.OutputValues)
                 {
-                    $fieldValue = $outputValue.psobject.Properties[$headerKey].Value
+                    $highlight = $outputValue.psobject.Properties['Highlight'].Value
+                    if ($highlight -ne $null -and $highlight.Count -gt 0)
+                    {
+                        $isAnItemError = $true
+                    }
+                }
+            }   
 
-                    It "Should not find any issues" {
-                        if ($outputValue.psobject.Properties['Highlight'].Value -ne $null -and $outputValue.psobject.Properties['Highlight'].Value.Contains($headerKey))
-                        {
-                            $global:issueFound = $true
+            if ($outputSection.NoIssuesFound -eq $false -and $isAnItemError -eq $false)
+            {
+                It "Should find no items for this Test" {
+                    $outputSection.OutputValues.Count | Should Be 0
+                }
+            } else {
+                foreach ($outputValue in $outputSection.OutputValues)
+                {
+                    #$cleanObject = $outputValue.psobject.Copy()
+                    #$cleanObject.psobject.Properties.Remove("Highlight")
+                    #$cleanJsonValue = ConvertTo-Json($cleanObject)
+                    $actualJsonValue = ConvertTo-Json($outputValue)
+                    
+                    foreach ($headerKey in $outputSection.OutputHeaders.Keys)
+                    {
+                        $fieldValue = $outputValue.psobject.Properties[$headerKey].Value
 
-                            $actualJsonValue | Should Not Be $actualJsonValue
-                        } else {
-                            $actualJsonValue | Should Be $actualJsonValue
+                        It "Should not find any issues" {
+                            if ($outputValue.psobject.Properties['Highlight'].Value -ne $null -and $outputValue.psobject.Properties['Highlight'].Value.Contains($headerKey))
+                            {
+                                $global:issueFound = $true
+
+                                $actualJsonValue | Should Not Be $actualJsonValue
+                            } else {
+                                $actualJsonValue | Should Be $actualJsonValue
+                            }
                         }
                     }
                 }
@@ -44,12 +66,10 @@ Function Invoke-OperationVerificationFrameworkScan
 
             # check now if an issue was found, but not in one of the items - it may be a test where the 
             # existence of items is itself the failure, like checking an error log
-            if ($global:issueFound -eq $false -and $outputSection.NoIssuesFound -eq $false)
-            {
-                It "Should find no values" {
-                    $outputSection.OutputValues.Count | Should Be 0
-                }
-            }
+            #if ($global:issueFound -eq $false -and $outputSection.NoIssuesFound -eq $false)
+            #{
+
+           # }
         }
     }
 
